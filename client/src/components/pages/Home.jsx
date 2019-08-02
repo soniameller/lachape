418c28e34a924eb923a5d11bc891de4ac555f037
Sonia Meller <mellersonia@gmail.com>
2019-08-02 15:08:55 +0100

    style

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -4,7 +4,10 @@ import Example from '../Carousel.jsx'
 export default function Home() {
   return (
     // <div className="Background-img--home" />
-    <div className="d-flex justify-content-center Background-img--home">
+    <div
+      style={{ backgroundColor: 'black' }}
+      className="d-flex justify-content-center"
+    >
       <Example />
     </div>
   )
